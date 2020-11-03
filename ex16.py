a2aef7959218a3848dd9bc4802e12b9f912bc484
cal-moh <calibremohit+github@gmail.com>
2020-11-03 10:05:06 +0530

    Added a try-except to this and was surprised - don't think it works on a single condition, as much as it works to run the function till an exception shows up anywhere

--- ex16.py
+++ ex16.py
@@ -3,33 +3,34 @@ from sys import argv
 script, filename = argv
 
 print(f"We're going to erase {filename}.")
-# Isn't there a way to handle this kind of Ctrl-C break with try-catch?
-
+# Isn't there a way to handle this kind of Ctrl-C break with try-catch? Attempting this with the input() function
 print("If you don't want that, hit CTRL=C (^C).")
 print("If you do want that, hit RETURN.")
 
-input("?")
+try: 
+    input("?")
+    print("Opening the file..")
+    target = open(filename, 'w')
+    # Interesting new function - truncate. I can also imagine there being some historical context to truncate, since the English meaning is clearly to stop, or cut off. Not to erase completely, which seems to be the meaning here.
+    print("Truncating the file. Goodbye!")
+    target.truncate()
 
-print("Opening the file..")
-target = open(filename, 'w')
-# Interesting new function - truncate. I can also imagine there being some historical context to truncate, since the English meaning is clearly to stop, or cut off. Not to erase completely, which seems to be the meaning here.
-print("Truncating the file. Goodbye!")
-target.truncate()
+    print("Now I'm going to ask you for three lines.")
 
-print("Now I'm going to ask you for three lines.")
+    line1 = input("line1: ")
+    line2 = input("line2: ")
+    line3 = input("line3: ")
 
-line1 = input("line1: ")
-line2 = input("line2: ")
-line3 = input("line3: ")
+    print("I'm going to write these to the file.")
 
-print("I'm going to write these to the file.")
+    target.write(line1)
+    target.write("\n")
+    target.write(line2)
+    target.write("\n")
+    target.write(line3)
+    target.write("\n")
 
-target.write(line1)
-target.write("\n")
-target.write(line2)
-target.write("\n")
-target.write(line3)
-target.write("\n")
-
-print("And finally, we close it.")
-target.close()
+    print("And finally, we close it.")
+    target.close()
+except KeyboardInterrupt:
+    print("Good call")
