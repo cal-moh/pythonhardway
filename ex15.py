ed06f39b06584036a760e20a06b7fe9a00690d51
cal-moh <calibremohit+github@gmail.com>
2020-11-03 10:03:53 +0530

    ex16 - file functions

--- ex15.py
+++ ex15.py
@@ -9,14 +9,23 @@ script, filename = argv
 
 # From here -  https://docs.python.org/3/tutorial/inputoutput.html - opening up a file with binary in read/write mode will mess around with line endings and convert them to \n (can be \r\n or \n when input, but they are converted). Which means that for some other types of files like JPEGs, there's a chance that the file will get corrupted. So should be opened up only in binary mode. Also, files should be closed manually to free up system resources. Or they should be opened as -- with open('workfile') as f:
 #    read_data = f.read()
+
+# So this is opening up this particular file in the default rt mode, and the variable txt is a stream now
 txt = open(filename)
 
 print(f"Here's your file {filename}:" )
+# The stream txt has a read() function which is converting next lines to \n everywhere.
 print(txt.read())
+txt.close()
 
 print("Type the filename again: ")
+# Taking a second filename as input and getting this name into txt_again
 file_again = input("> ")
+# The stream txt_again is a stream that does the same thing as the txt stream, with all operations available here too
 
 txt_again = open(file_again)
-
+# Attempted this function on a file stream and this was interesting - the open() only opens up the stream, and does nothing else. It is then up to the read() or write() functions to pull or push data. open() probably only brings it up to something like RAM. Or, better, it's like setting a stereo up to play the cassette. The stereo can play the cassette, but the open() only loads the cassette into it. 
+# txt_again.close()
 print(txt_again.read())
+
+txt_again.close()
---
+++ ex16.py
@@ -0,0 +1,35 @@
+from sys import argv
+# I like this idea of assigning script every time to an argv zeroth argument, for it stops me from making any error because of an incorrect script name
+script, filename = argv
+
+print(f"We're going to erase {filename}.")
+# Isn't there a way to handle this kind of Ctrl-C break with try-catch?
+
+print("If you don't want that, hit CTRL=C (^C).")
+print("If you do want that, hit RETURN.")
+
+input("?")
+
+print("Opening the file..")
+target = open(filename, 'w')
+# Interesting new function - truncate. I can also imagine there being some historical context to truncate, since the English meaning is clearly to stop, or cut off. Not to erase completely, which seems to be the meaning here.
+print("Truncating the file. Goodbye!")
+target.truncate()
+
+print("Now I'm going to ask you for three lines.")
+
+line1 = input("line1: ")
+line2 = input("line2: ")
+line3 = input("line3: ")
+
+print("I'm going to write these to the file.")
+
+target.write(line1)
+target.write("\n")
+target.write(line2)
+target.write("\n")
+target.write(line3)
+target.write("\n")
+
+print("And finally, we close it.")
+target.close()
