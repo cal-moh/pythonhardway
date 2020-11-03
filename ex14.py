08b1eea9d1639660daf24fb0533988d924ce8095
cal-moh <calibremohit+github@gmail.com>
2020-11-03 07:59:44 +0530

    Forced push to git documented in ex14

--- ex14.py
+++ ex14.py
@@ -4,6 +4,7 @@ script, user_name, workplace = argv
 # I see how handy this is. Pretty cool - I like that I can change it here and not worry about changing it everywhere. I also see how various command prompts can be made to work. Very interesting. 
 
 # Set this up to the git repository called pythonhardway. Some struggle there because git remote had to be reset, old credentials had to be deleted, and new credentials had to be put in. Will attempt to write everything in a git update sort of way. https://stackoverflow.com/questions/10904339/github-fatal-remote-origin-already-exists
+# More pain here - the first time after a rebase a forced push is needed to Git - https://stackoverflow.com/questions/39399804/updates-were-rejected-because-the-tip-of-your-current-branch-is-behind-its-remot
 prompt = '\\ '
 
 print(f"Hi {user_name}, I'm the {script} script.")
